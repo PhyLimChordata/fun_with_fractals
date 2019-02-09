f32f67456d07d16d9fbdc151529eaf23f19c5525
Andy Phy Lim <36771187+PhyLimChordata@users.noreply.github.com>
2019-02-09 14:22:48 -0500

    Update Fractal.py

--- Fractal.py
+++ Fractal.py
@@ -1,16 +1,13 @@
 import turtle
 
-# drawer
+# drawer set up
 loadWindow = turtle.Screen()
 loadWindow.screensize(1000,1000)
 drawer = turtle.Pen()
 drawer.speed(100)
 drawer.penup()
-# picks up the pen (not to draw anything)
 drawer.goto(-300, 100)
-# the pen goes to this location with the
 drawer.pendown()
-# the pen is now down so it can draw now
 
 
 # defining our function that draws +
@@ -18,16 +15,12 @@ def draw_fractal(length, depth):
     drawer.forward(length)
     if depth > 1:
         drawer.backward(length / 2)
-        # after drawing forward goes to the midpoint of the line
         drawer.left(90)
-        # rotates the drawer (does the top part of +)
         draw_fractal(length/1.6, depth - 1)
-        # recursively draws more crosshairs off the branching crosshair
         drawer.right(180)
         draw_fractal(length/1.6, depth - 1)
         drawer.right(90)
         drawer.forward(length / 2)
-        # goes back to original position
     drawer.right(180)
     drawer.forward(length)
 
